eec274c762f2ce16a4d91089a1a3507cdf562064
ahmedjhin <a7madti90@hotmail.com>
2024-08-05 23:56:48 +0400

    first

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+const AboutPage = () =>{
+    return (
+        <>
+        <div>AboutPage skrrrrrrt</div>
+        <button> <a href="/"> go back</a></button>
+        </>
+    )
+}
+
+
+export default AboutPage
